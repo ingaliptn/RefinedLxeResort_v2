dc3ca568bf971dd611b2cfd9981f9720f47e0b56
Mykyta Tkachuk <nikitatkachuk6@gmail.com>
2024-05-24 23:29:31 +0300

    .

---
+++ RefinedLuxeResortV2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RefinedLuxeResortV2.Models;
+
+namespace RefinedLuxeResortV2.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult News_Blog_Details()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
